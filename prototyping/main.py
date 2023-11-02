e8d5e5b12ffd09e022fd3116d277103bb9de0172
kgranger777 <kgranger777@gmail.com>
2023-11-02 13:00:49 -0500

    Begin prototyping for FancyDoor. Currently testing: - Captcha generation and validation - System-wide debug logging Up next: - Image recognition - Image capture from camera - Project state machine

---
+++ prototyping/main.py
@@ -0,0 +1,53 @@
+# Build state machine for door
+# Features:
+#   Retina Scan.
+#   Hand Scan.
+#   Triple layered keycode lock.
+#   - 4-key pin lock
+#   - 8-key pin lock
+#   - 16-key pin lock
+#   Voice activation.
+#   Distance detection activation.
+#   GUI driven captcha.
+#   Digital logic gate driven input combination.
+#   Servo controlled locks.
+#   Door will open (manually).
+
+# WHAT DOES ANY OF THIS MEAN
+import logging
+import random
+import sys
+from captcha.image import ImageCaptcha
+
+
+# NOTE: CHANGE LOGGING LEVEL HERE
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+# logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
+
+
+# Given length of captcha, generate a captcha and return generated text
+def generate_captcha(length):
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    captcha_text = "".join(random.choice(characters) for _ in range(length))
+    logging.debug("Generated captcha: " + captcha_text)
+    image = ImageCaptcha(width=300, height=150, fonts=['assets/times.ttf', 'assets/lucon.ttf'])
+    image.generate(captcha_text)
+    image.write(captcha_text, 'assets/CAPTCHA.png')
+    return captcha_text
+
+
+def captcha_handler():
+    captcha_text = generate_captcha(8).lower()
+    user_input = input("Captcha validation: ").lower()
+    if captcha_text == user_input:
+        print("Success")
+    else:
+        print("Incorrect captcha")
+
+
+def main():
+    captcha_handler()
+
+
+if __name__ == "__main__":
+    main()

6decd11e438cda9600338d7aba1121e5d28770b6
kgranger777 <kgranger777@gmail.com>
2023-11-25 15:26:14 -0600

    Main functionality commit for final project

--- prototyping/main.py
+++ prototyping/main.py
@@ -19,10 +19,38 @@ import os
 import random
 import sys
 import tkinter as Tk
+import cv2
+import time
+import RPi.GPIO as GPIO
+import sounddevice as sd
+import numpy as np
 
 from PIL import ImageTk, Image
 from captcha.image import ImageCaptcha
 from imageai.Detection import ObjectDetection
+
+# Perform GPIO Setup
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+# Set up pins for servo and start PWM
+entrypin = 21
+motorpin = 19
+GPIO.setup(entrypin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(motorpin, GPIO.OUT)
+freq = 50
+p = GPIO.PWM(motorpin, freq)
+p.start(5)
+
+# Set up pins for ultrasonic sensor
+trigger = 11
+echo = 13
+GPIO.setup(trigger, GPIO.OUT)
+GPIO.setup(echo, GPIO.IN)
+
+
+# Declare global image path
+image_path = "assets/image.jpg"
 # Set up new logger for use in debugging
 logger = logging.getLogger("debug_logger")
 
@@ -48,9 +76,111 @@ def generate_captcha(length):
     logger.debug("Generated captcha: " + captcha_text)
     image = ImageCaptcha(width=300, height=150, fonts=['assets/times.ttf', 'assets/lucon.ttf'])
     image.generate(captcha_text)
-    path = 'assets/CAPTCHA.png'
+    path = "assets/CAPTCHA.png"
     image.write(captcha_text, path)
     return captcha_text.lower(), path
+
+# Get an image from the video capture device
+def get_image():
+    global image_path
+    try:
+        cam = cv2.VideoCapture(0)
+        while True:
+            ret, image = cam.read()
+            cv2.imshow('Imagetest',image)
+            k = cv2.waitKey(1)
+            if k != -1:
+                break
+        cv2.imwrite(image_path, image)
+        cam.release()
+        cv2.destroyAllWindows()
+    except cv2.error:
+        return False
+    return True
+
+
+def get_sound(indata, outdata, frames, time, status):
+    global sound_val 
+    sound_val = np.linalg.norm(indata)*10
+
+
+# Listen for voice activation
+def listen(max_delta):
+    global sound_val
+    delta = 0
+    prev_sound_val = 0
+    try:
+        while delta < max_delta:
+            with sd.Stream(callback=get_sound):
+                sd.sleep(50)
+            delta = abs(sound_val - prev_sound_val)
+            print(delta)
+            prev_sound_val = sound_val
+        return True
+    except: 
+        return False
+
+
+# Get distance from ultrasonic sensor
+def distance():
+    logger.debug("Called distance function")
+    # set Trigger to HIGH
+    GPIO.output(trigger, True)
+    logger.debug("Set trigger high")
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(trigger, False)
+    logger.debug("Set trigger low")
+
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(echo) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(echo) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+    logger.debug("Distance " + str(distance))
+    return distance
+
+
+# image_recognizer
+# Params: image_path    A string path to an image
+# Returns: results      A tuple consisting of the prediction name and percent certainty
+def image_recognizer(image_path):
+    logger.debug("Recognizer called with path " + image_path + " , setting up model...")
+    execution_path = os.getcwd()
+    detector = ObjectDetection()
+
+    # NOTE Change this to use different models
+    # detector.setModelTypeAsRetinaNet()
+    detector.setModelTypeAsYOLOv3()
+    # detector.setModelTypeAsTinyYOLOv3()
+
+    # NOTE Path for Windows
+    # detector.setModelPath(execution_path + "\\assets\\retinanet_resnet50_fpn_coco-eeacb38b.pth")
+    # NOTE Path for Linux (RasPi)
+    # detector.setModelPath(execution_path + "/assets/retinanet_resnet50_fpn_coco-eeacb38b.pth")
+    detector.setModelPath(execution_path + "/assets/yolov3.pt")
+    # detector.setModelPath(execution_path + "/assets/tiny-yolov3.pt")
+
+    detector.loadModel()
+    detector.useCPU()
+    logger.debug("Model loaded successfully, performing image detection...")
+    predictions = detector.detectObjectsFromImage(input_image=image_path, minimum_percentage_probability=30)
+    results = []
+    for prediction in predictions:
+        results.append((prediction['name'], prediction['percentage_probability']))
+    logger.debug("Detection finished. Returning results.")
+    return results
 
 
 # Application class handles GUI
@@ -61,7 +191,7 @@ class Application(Tk.Frame):
         self.captchaUserInput = Tk.StringVar()
         self.pin = Tk.StringVar()
         self.grid(sticky='nsew')
-        self.createWidgets()
+        self.createStartingWidgets()
 
     # Destroys all widgets in application
     def clearFrame(self):
@@ -77,6 +207,19 @@ class Application(Tk.Frame):
 
         self.quitButton = Tk.Button(self, text="Quit", command=self.quit)
         self.quitButton.grid(sticky='ew', padx=(100, 100), pady=(5, 50))
+
+
+    def unlock(self):
+        logger.debug("Setting up unlock screen")
+        # Function called in callback from servo motor function
+        self.clearFrame()
+        self.successLabel = Tk.Label(self, text="Congration, you done it")
+        self.successLabel.grid(sticky='ew', padx=(100, 100), pady=(5, 50))
+
+        self.okButton = Tk.Button(self, text="OK", command=self.quit)
+        self.okButton.grid(sticky='ew', padx=(100, 100), pady=(5, 50))
+        GPIO.cleanup()
+
 
     # Captcha submission function validates captcha and sets up window for pin entry
     def submitCaptcha(self):
@@ -126,7 +269,7 @@ class Application(Tk.Frame):
 
 
     def logicGateSetup(self):
-        logger.debug("Setting up logic gate text screen")
+        logger.debug("Setting up logic gate prompt frame")
         # Clear frame and set up next state
         self.clearFrame()
         self.instructionLabel = Tk.Label(self, text="Success")
@@ -135,21 +278,21 @@ class Application(Tk.Frame):
         self.logicGateInstructions.grid(sticky='ew', padx=(100, 100), pady=(5, 5))
         self.quitButton = Tk.Button(self, text="Quit", command=self.quit)
         self.quitButton.grid(sticky='ew', padx=(100, 100), pady=(5, 50))
+        def callback(channel):
+            global p
+            p.ChangeDutyCycle(10.2)
+            time.sleep(1.5)
+            logger.debug("\nDOOR UNLOCKED\n")
+            self.unlock()
 
-        # TODO call function to set up listener for logic gate puzzle here - basically all we need to do is have
-        #  some statement here that says, "if we receive a signal on some pin, update the GUI with success
-        #  message and call the servo function"
-        # Algorithm:
-        # 1. Call function to set up pin signal listener (prevents any chance of pin being activated before this
-        # state is reached)
-        # 2. If pin signalled, call servo setup function (prevents any chance of any other code referencing the servo
-        # before this state is reached)
-        #       - Can either control the servo activation here or there
-        # 4. Update GUI with success message - "door" is now open
-        logger.debug("TODO: Call function to set up listener for logic gate puzzle here...\n\n")
+        logger.debug("Setting up event detector for logic gate puzzle")
+        GPIO.add_event_detect(entrypin, GPIO.RISING, callback = callback, bouncetime = 300)
+        
 
 
-    def createWidgets(self):
+    def captchaSetup(self):
+        logger.debug("Setting up Captcha frame")
+        self.clearFrame()
         # User instructions
         self.instructionLabel = Tk.Label(self, text="Enter Captcha:")
         self.instructionLabel.grid(sticky='ew', padx=(100, 100), pady=(50, 5))
@@ -172,88 +315,109 @@ class Application(Tk.Frame):
         self.quitButton.grid(sticky='ew', padx=(100, 100), pady=(5, 5))
 
 
+    def imageScanSuccess(self):
+        logger.debug("Setting up transient image recognition screen")
+        global image_path
+        self.clearFrame()
+        self.infoLabel = Tk.Label(self, text="Please wait, recognizing image...")
+        self.infoLabel.grid(sticky='ew', padx=(100, 100), pady=(50, 5))
+        self.quitButton = Tk.Button(self, text="Quit", command=self.quit)
+        self.quitButton.grid(sticky='ew', padx=(100, 100), pady=(5, 5))
+        self.update()
+        #time.sleep(.5)
+        results = image_recognizer(image_path)
+        for name, percentage in results:
+            logger.debug("Recognizing " + name + " with certainty " + str(percentage))
+            if name == 'person' and percentage > 60:
+                logger.debug("Succeeded image recognition")
+                self.infoLabel.configure(text="Image recognition success.\nPlease proceed to the next step.")
+                self.update()
+                time.sleep(3)
+                return True
+        logger.debug("Failed image recognition")
+        return False
+
+
+    def handScanSetup(self):
+        logger.debug("Setting up hand scanner frame")
+        self.clearFrame()
+        self.instructionLabel = Tk.Label(self, text="Hand Scan: Press any key to capture image")
+        self.instructionLabel.grid(sticky='ew', padx=(100, 100), pady=(50,5))
+
+        self.quitButton = Tk.Button(self, text="Quit", command=self.quit)
+        self.quitButton.grid(sticky='ew', padx=(100, 100), pady=(5, 5))
+        self.update()
+        #time.sleep(.5)
+        return_val = get_image()
+        if return_val:
+            logger.debug("Image created successfully")
+            if self.imageScanSuccess():
+                logger.debug("Image scanned successfully")
+                self.captchaSetup()
+            else:
+                logger.debug("Hand scan failed")
+                self.lock()
+        else:
+            self.lock()
+
+
+    def retinaScanSetup(self):
+        logger.debug("Setting up retina scanner frame")
+        self.clearFrame()
+        self.instructionLabel = Tk.Label(self, text="Retina Scan: Press any key to capture image")
+        self.instructionLabel.grid(sticky='ew', padx=(100, 100), pady=(50,5))
+
+        self.quitButton = Tk.Button(self, text="Quit", command=self.quit)
+        self.quitButton.grid(sticky='ew', padx=(100, 100), pady=(5, 5))
+        self.update()
+        #time.sleep(.5)
+        return_val = get_image()
+        if return_val:
+            logger.debug("Image created successfully")
+            if self.imageScanSuccess():
+                logger.debug("Image scanned successfully")
+                self.handScanSetup()
+            else:
+                logger.debug("Retina scan failed")
+                self.lock()
+        else:
+            self.lock()
+
+
+    def createStartingWidgets(self):
+        logger.debug("Setting up starting widgets (TODO Voice validation)")
+        self.instructionLabel = Tk.Label(self, text="Voice validation needed")
+        self.instructionLabel.grid(sticky='ew', padx=(100, 100), pady=(50, 5))
+
+        self.quitButton = Tk.Button(self, text="Quit", command=self.quit)
+        self.quitButton.grid(sticky='ew', padx=(100, 100), pady=(5, 5))
+        # Force update before calling logic
+        self.update()
+
+        # Start listening for audio at threshold 100
+        logger.debug("Listening for voice")
+        voice_validation = listen(100)
+
+        # Get current distance
+        logger.debug("Getting distance")
+        proximity = distance()
+
+        # Call retina scan if voice and proximity thresholds passed
+        if voice_validation and proximity < 30:
+            self.retinaScanSetup()
+        else:
+            self.lock()
+
+
 root = Tk.Tk()
 # Initialize app
 app = Application()
 
 
-# image_recognizer
-# Params: image_path    A string path to an image
-# Returns: results      A tuple consisting of the prediction name and percent certainty
-def image_recognizer(image_path):
-    logger.debug("Recognizer called with path " + image_path + " , setting up model...")
-    execution_path = os.getcwd()
-    detector = ObjectDetection()
-
-    # NOTE Change this to use different models
-    # detector.setModelTypeAsRetinaNet()
-    detector.setModelTypeAsYOLOv3()
-    # detector.setModelTypeAsTinyYOLOv3()
-
-    # NOTE Path for Windows
-    # detector.setModelPath(execution_path + "\\assets\\retinanet_resnet50_fpn_coco-eeacb38b.pth")
-    # NOTE Path for Linux (RasPi)
-    # detector.setModelPath(execution_path + "/assets/retinanet_resnet50_fpn_coco-eeacb38b.pth")
-    detector.setModelPath(execution_path + "/assets/yolov3.pt")
-    # detector.setModelPath(execution_path + "/assets/tiny-yolov3.pt")
-
-    detector.loadModel()
-    detector.useCPU()
-    logger.debug("Model loaded successfully, performing image detection...")
-    predictions = detector.detectObjectsFromImage(input_image=image_path, minimum_percentage_probability=30)
-    results = []
-    for prediction in predictions:
-        results.append((prediction['name'], prediction['percentage_probability']))
-    logger.debug("Detection finished. Returning results.")
-    return results
-
-
-# Note I was gonna do this whole thing where the state gets passed from method to method but that wasn't gonna work
-#  in practice with the GUI. Now, the GUI handles state management of itself and interacts with external methods only
-#  to interface with external hardware or libraries (captcha, imageai, servo control, etc). So for now these lines
-#  are defunct
-# Retina and hand scanner - should open camera, take a picture, save the picture, and call image_recognizer with the
-# image path. Will update state depending on success or fail.
-# NOTE difference between the scanner and the recognizer is that the scanner handles state.
-'''
-def scanner(state):
-    # TODO Wrap in "if" block when state known
-    if state:
-        # TODO Get hand image from camera. Take a picture, save image & its path, and pass path to image_recognizer()
-        # Recognize image
-        result = image_recognizer("path/to/image")
-        # Result is good if it detects a person with more than 60% certainty
-        for name, percent in result:
-            if name == 'person' and percent > 60:
-                pass
-            else:
-                # TODO Figure out state control...
-                break
-
-        # Get eye image from camera
-        # Recognize image
-        result = image_recognizer("path/to/image")
-        # Result is good if it detects a person with more than 60% certainty
-        for name, percent in result:
-            if name == 'person' and percent > 60:
-                state += 1
-    else:
-        # If image recognition failed,
-        state = 0
-'''
-
-
 def main():
     global app
-    # Disable this to only test image recognition
     app.master.title("Fancy Door")
     app.mainloop()
-
-    path = "assets/hand.jpg"
-    results = image_recognizer(path)
-    print("Image predictions: ")
-    for name, percentage in results:
-        print(" -", name, "with certainty", percentage)
 
 
 if __name__ == "__main__":

ea3c79d94e04551adf5a0b17b504e5c9d24c83c0
SammyBoiBeThicc <afunsamboss@gmail.com>
2023-11-24 21:56:40 -0600

    Added functional code applicable to our project for the microphone functions

--- prototyping/Test Microphone/test.py
+++ prototyping/Test Microphone/test.py
@@ -1,11 +1,28 @@
-# Print out realtime audio volume as ascii bars
-
 import sounddevice as sd
 import numpy as np
 
-def print_sound(indata, outdata, frames, time, status):
-    volume_norm = np.linalg.norm(indata)*10
-    print ("|" * int(volume_norm))
 
-with sd.Stream(callback=print_sound):
-    sd.sleep(10000)
+def print_sound(indata, outdata, frames, time, status):
+    global sound_val 
+    sound_val = np.linalg.norm(indata)*10
+
+def main():
+    global sound_val
+    delta = 0
+    max_delta = 100
+    prev_sound_val = 0
+
+    try:
+        while delta < max_delta:
+            with sd.Stream(callback=print_sound): 
+                sd.sleep(50)
+            delta = abs(sound_val - prev_sound_val)
+            print(delta)
+            prev_sound_val = sound_val
+        print(f'Done.') 
+    except: 
+        print('\nExiting.')
+
+
+if __name__=='__main__':
+    main()
